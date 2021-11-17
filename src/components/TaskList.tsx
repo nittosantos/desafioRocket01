19806013a10c90df99b779572abc44afee6d8a95
wellyngtonTC <wellyngton.santos@externo.tc.com.br>
2021-11-16 21:00:36 -0300

    :tada: Melhorando o desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -24,7 +24,7 @@ export function TaskList() {
       isComplete: false,
     };
 
-    setTasks((oldState) => [...tasks, newTask]);
+    setTasks([...tasks, newTask]);
     setNewTaskTitle('');
   }
 
